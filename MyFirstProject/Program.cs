f635307e27fec0bf89db8c03f5c9aad9669a1659
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2025-06-16 21:32:17 +0300

    Initial commit

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,17 @@
+﻿// Burası bir yorum satırıdır...
+
+Console.WriteLine("Merhaba Dünya");
+Console.WriteLine("----------------");
+
+string message = null;
+Console.WriteLine("Metin Giriniz: ");
+message = Console.ReadLine();
+
+Console.WriteLine("Metininiz: " + message);
+
+Console.WriteLine("----------------");
+
+string ad = "Ahmet";
+var soyad = "Güvendik"; 
+
+Console.WriteLine(ad + " " + soyad);
