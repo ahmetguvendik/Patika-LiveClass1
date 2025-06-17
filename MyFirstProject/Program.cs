f7c47a2a72efd6567d9248aca0dc8cbe5cf3530c
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2025-06-17 10:52:03 +0300

    update

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Burası bir yorum satırıdır...
 
 
-string ad = "Ahmet";
-var soyad = "Güvendik";
+string ad = "Ahmet";  //değişken tanımlaması string
+var soyad = "Güvendik"; //var değişken türü otomatik olarak yazılan değerin türünü bilir ve onu alır
 string message = "Patika sayesinde eksiklerimi kapatacağıma ve sektöre atılacağıma inandığım için buradayım";
 
-Console.WriteLine(ad + " " + soyad);
-Console.WriteLine(message);
+Console.WriteLine(ad + " " + soyad);  //değişkenleri burada string türünde birleştirerek yazıyoruz.
+Console.WriteLine(message); 
